9cc549ea857db7358d0ae02a266650ff7769ee0b
Silver <Silverg33k@gmail.com>
2023-11-23 14:19:37 +0100

    .......

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
+const { WebClient } = require('@slack/web-api');
 const api = express()
+
+const slack = new WebClient(process.env.SLACK_TOKEN, {});
 
 
 const PORT = process.env.PORT;
+const channelId = process.env.SLACK_CHANNEL_ID;
+
 
 api.use(express.json())
 api.use(express.urlencoded({ extended: true }))
@@ -23,9 +28,33 @@ api.use(express.urlencoded({ extended: true }))
 
 
 //require major routes
-api.post('/message', (request, response) => {
+api.post('/message', async (request, response) => {
     const { message } = request.body;
-    response.status(200).send(`Message posted ${message}`);
+
+    // Send message
+    try {
+        const posted = await slack.chat.postMessage({
+            text: message,
+            channel: channelId,
+        });
+    
+        if(!posted) {
+            response.status(500).json({
+                status: false,
+                message: 'Unable to post message at this time'
+            });
+        } 
+    
+        response.status(200).json({
+            status: true,
+            message: 'Message posted'
+        });
+    } catch (e) {
+        response.status(400).json({
+            status: false,
+            message: 'System error, try again later'
+        });
+    }
 })
 
 api.get('/', (request, response) => {
@@ -33,13 +62,21 @@ api.get('/', (request, response) => {
 })
 
 api.get('/stat', (request, response) => {
-    response.status(200).send('233 people just clicked on the link');
+    response.status(200).json({
+        status: true,
+        message: 'System stats retrieved',
+        data: {
+            clicks: '33 people just clicked on the link'
+        }
+    });
 })
 
 api.all('*', (request, response) => {
-    response.status(200).send('Ghost route has ghosted');
+    response.status(404).json({
+        status: false,
+        message: 'Ghost route has ghosted'
+    });
 });
-
 
 api.listen(PORT, ()=> {
   console.log(`Ghost listening on port ${PORT}`)
